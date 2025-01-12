f5531978a83e8521adbc6eb93570823b8669f915
gupta-rajan <rjngupta22@gmail.com>
2025-01-12 12:15:27 +0530

    prepare for production

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,17 @@ import productRoutes from './routes/productRoutes.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
+
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 connectDB(); // Connect to the MongoDB database
 
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
 
 // app.get('/api/products', (req, res) => {
 //     res.json(products);
@@ -27,5 +27,17 @@ app.get('/', (req, res) => {
 app.use('/api/products',productRoutes);
 app.use(notFound);
 app.use(errorHandler);
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}else{
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
